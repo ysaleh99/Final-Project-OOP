9c3e9636744db1c5db8e0f51d6423349fdd598e0
ysaleh99 <58055520+ysaleh99@users.noreply.github.com>
2019-11-21 19:32:57 -0500

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,44 @@
+package Week_3;
+
+public class Student {
+	private String name;
+	private int student_id;
+	private double balance;
+	
+	public Student() {
+		name="";
+		student_id=0;
+		balance=0.0;
+	}
+	
+	public Student(String name, int student_id) {
+		this.name = name;
+		this.student_id = student_id;
+		balance = 0.0;
+	}
+
+	public int getStudentID() {
+		return student_id;
+	}
+
+	public void setStudentID(int number) {
+		this.student_id = number; 
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void deposit(double amount) {
+		this.balance = balance + amount;
+	}
+	
+	public double getBalance() {
+		return balance;
+	}
+
+}
---
+++ StudentTester.java
@@ -0,0 +1,83 @@
+package Week_3;
+
+import java.util.Scanner;
+
+public class StudentTester {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Student student1 = new Student();
+		Student student2 = new Student("Alex Alvero", 180005066);
+		Student student3 = new Student();
+		/*
+		int id = student1.getStudentID();
+		String name = student1.getName();
+		System.out.println("ID 1: " + id);
+		System.out.println("Name 1: " + name);
+		 */
+		student3.setName("Youssef Saleh");
+		student3.setStudentID(180007048);
+		System.out.println("Student Name: " + student3.getName());
+		System.out.println("StudentID: " + student3.getStudentID());
+		System.out.println();
+		
+		Scanner in = new Scanner(System.in);
+		System.out.print("Would you like to check balance or deposit money? (please type \"balance\" or \"deposit\", and press enter): ");
+		boolean checker = false;
+		do {
+			String ans = in.nextLine();
+		
+			if (ans.equals("deposit")) {
+				System.out.print("Enter amount of money to deposit: ");
+				double amount = in.nextDouble();
+				student3.deposit(amount);
+				System.out.print("Your balance is: $" + student3.getBalance());
+				boolean checker3 = false;
+				System.out.println();
+				System.out.print("To get back to the previous menu type \"prev\", and to exit type \"exit\": ");
+				while (checker3==false) {
+					String choose2 = in.nextLine();
+					if (choose2.equals("prev")) {
+						checker3 = true;
+						checker=false;
+						System.out.print("Would you like to check balance or deposit money? (please type \"balance\" or \"deposit\", and press enter: ");
+					} else if (choose2.equals("exit")) {
+						checker3 = true;
+						checker = true;
+					} else {
+						checker3 = false;
+					} 
+				} 				
+			} else if (ans.equals("balance")) {
+				System.out.println("Your balance is: $" + student3.getBalance());
+				boolean checker2 = false;
+				do {
+					System.out.println();
+					System.out.print("To get back to the previous menu type \"prev\", and to exit type \"exit\": ");
+					String choose = in.nextLine();
+					if (choose.equals("prev")) {
+						checker2 = true;
+						checker=false;
+						System.out.print("Would you like to check balance or deposit money? (please type \"balance\" or \"deposit\", and press enter): ");
+					} else if (choose.equals("exit")) {
+						checker2 = true;
+						checker = true;
+					} else {
+						checker2 = false;
+					} 
+				} while (checker2==false);
+
+			} else {
+				System.out.print("Error. please type \"balance\" or \"deposit\", and press enter: ");
+				checker=false;
+			} 
+		} while (checker==false);
+		
+		System.out.println("Thank you for using our system!");
+		/*
+		int id2 = student2.getStudentID();
+		String name2 = student2.getName();
+		System.out.println("ID 2: " + id2);
+		System.out.println("Name 2: " + name2);
+		*/
+	}
+}
